94513d1ef3b9adacfb92318cfc840b1bea09bd24
aletheia <aletheiak93@gmail.com>
2021-12-28 13:29:01 -0800

    phase 1 completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import harvardArt from './data/harvardArt';
+import GalleryNavigation from './components/GalleryNavigation';
 
 function App() {
   // console.log(harvardArt);
 
   return (
     <div>
-      <h1>Hello from App</h1>
+      <GalleryNavigation galleries={harvardArt.records}/>
     </div>
   );
 }
