92409976a4d5f56c4154870dea4f2a8b462a5482
Savanah Trewman <strewman@me.com>
2021-12-28 15:06:09 -0600

    finished phase 0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import harvardArt from './data/harvardArt';
 
 function App() {
+  // console.log(harvardArt);
+
   return (
     <div>
-      <h1>React App</h1>
+      <h1>Hello from App</h1>
     </div>
   );
 }
